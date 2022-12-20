7db93f0e47250ee9f28460b83b7c7ba31893d856
EBADURRAHMAN <112888726+Ebad-1999@users.noreply.github.com>
2022-12-20 22:44:24 +0430

    JSV6

---
+++ JavaScriptDt-master/assets/JS/61-event-handling-practice-1.js
@@ -0,0 +1,37 @@
+const btnAverage = document.querySelector("#btnAverage");
+
+const findAverage = () => {
+    const grade1 = document.querySelector("#grade1").value;
+    const grade2 = document.querySelector("#grade2").value;
+
+    const average = getAverage(grade1, grade2);
+    const letter = convertNumberToLetter(average);
+    alert(letter);
+}
+
+btnAverage.addEventListener("click", findAverage);
+
+// isNaN(variable);
+
+// parseInt(), parseFloat()
+// Number(x) convert x to a numerical value, if its empty 0;
+// parseFloat(x) converts to a decimal value, if its empty spaces returned to NaN
+
+const getAverage = (num1, num2) => {
+    if (isNaN(parseFloat(num1)) || isNaN(parseFloat(num2))) return;
+    const average = (parseFloat(num1) + parseFloat(num2)) / 2;
+    return average;
+};
+
+
+const convertNumberToLetter = (num) => {
+    if (isNaN(parseFloat(num))) return;
+    let letter;
+
+    if (num >= 90 && num <= 100) letter = "A"
+    else if (num >= 80 && num < 90) letter = "B"
+    else if (num >= 70 && num < 80) letter = "C"
+    else if (num >= 50 && num < 70) letter = "D"
+    else letter = "F"
+    return letter;
+};
